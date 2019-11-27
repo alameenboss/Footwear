7689915e41952dbf454612b870b1f703207d4158
Alameen <4115051+alameenboss@users.noreply.github.com>
2019-11-27 08:52:02 +0530

    Implement Random for generating Rate,star and stared count

--- Footwear/HelperUtility.cs
+++ Footwear/HelperUtility.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.IO;
+using System.Security.Cryptography;
 
 namespace Footwear
 {
@@ -12,6 +13,17 @@ namespace Footwear
         {
             Random random = new Random();
             return random.Next(min, max);
+        }
+
+        public static int GetTrueRandom()
+        {
+            int value = 1;
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                byte[] randomNumber = new byte[4];
+                rng.GetBytes(randomNumber);
+                return value = BitConverter.ToInt32(randomNumber, 0);
+            }
         }
 
         public static string GetBaseDirectory()
@@ -50,6 +62,42 @@ namespace Footwear
             });
 
             return imageList;
+        }
+    }
+
+    public static class RandomNumber
+    {
+        private static Random r = new Random();
+        private static object l = new object();
+        private static Random globalRandom = new Random();
+        [ThreadStatic]
+        private static Random localRandom;
+        public static int GenerateNewRandom(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
+        public static int GenerateLockedRandom(int min, int max)
+        {
+            int result;
+            lock (RandomNumber.l)
+            {
+                result = RandomNumber.r.Next(min, max);
+            }
+            return result;
+        }
+        public static int GenerateRandom(int min, int max)
+        {
+            Random random = RandomNumber.localRandom;
+            if (random == null)
+            {
+                int seed;
+                lock (RandomNumber.globalRandom)
+                {
+                    seed = RandomNumber.globalRandom.Next();
+                }
+                random = (RandomNumber.localRandom = new Random(seed));
+            }
+            return random.Next(min, max);
         }
     }
 }
