c92ea69d5b28d41e066a199768d8004ef068d2b2
Alameen <4115051+alameenboss@users.noreply.github.com>
2019-11-28 00:38:12 +0530

    Implement Login and Register

---
+++ Footwear/Controllers/AccountController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Footwear.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string Username,string Password)
+        {
+            if (Username == "admin" && Password == "admin")
+            {
+                return RedirectToAction("Shop", "Home");
+            }  
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(string Name, string Password)
+        {
+            if (Name == "alameen" && Password == "alameen")
+            {
+                return RedirectToAction("Shop", "Home");
+            }
+            return View();
+        }
+    }
+}
